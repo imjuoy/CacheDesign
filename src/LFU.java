71a997f3d2ede4a71ffc46a26bfd29114ed3bb12
imjuoy <bsubhadeep95@gmail.com>
2018-05-30 03:13:09 -0400

    Least Frequently Used test

---
+++ src/LFU.java
@@ -0,0 +1,19 @@
+/**
+ * Least Frequently Used Cache
+ * 
+ * @author subhadeepbhattacharyya
+ *
+ */
+
+public class LFU {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int s = '1' - '0';
+
+		System.out.println(s);
+
+	}
+
+}
