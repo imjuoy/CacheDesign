d74f3141efc72531ac0df5a2dbaaa9b02deb6776
imjuoy <bsubhadeep95@gmail.com>
2018-05-30 11:11:32 -0400

    Random changes

--- src/LFU.java
+++ src/LFU.java
@@ -10,9 +10,10 @@ public class LFU {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int s = '1' - '0';
+		// int s = '1' - '0';
+		int a = 'a';
 
-		System.out.println(s);
+		System.out.println(a);
 
 	}
 
