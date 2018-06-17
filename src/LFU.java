57d6166c393cd94c2b2dd8c77870af7ff5c8ec25
imjuoy <bsubhadeep95@gmail.com>
2018-06-16 23:57:06 -0400

    LFU intial design

--- src/LFU.java
+++ src/LFU.java
@@ -1,4 +1,8 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
 
 /**
  * Least Frequently Used Cache
@@ -9,11 +13,26 @@ import java.util.Arrays;
 
 public class LFU {
 
+	HashMap<Integer, Integer> cache;
+	List<Integer> innerList;
+	int capacity;
+
+	public LFU(int capacity) {
+		this.cache = new LinkedHashMap<>();
+		this.innerList = new ArrayList<>();
+		this.capacity = capacity;
+	}
+
+	public void put(int key, int val) {
+	}
+
+	public int get(int key) {
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	
-		}
 	}
+}
 
 }
