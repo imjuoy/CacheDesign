f1a5b089e8b2a48f5661b0d45ce3539424866f33
imjuoy <bsubhadeep95@gmail.com>
2018-06-25 23:56:38 -0400

    Find Second Max in an Array

---
+++ src/FindSecondMaxInAnArray.java
@@ -0,0 +1,23 @@
+
+public class FindSecondMaxInAnArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] input = { 1, 2, 3 };
+		Integer max1 = null;
+		Integer max2 = null;
+
+		for (Integer num : input) {
+			if (num.equals(max1) || num.equals(max2))
+				continue;
+			if (max1 == null || num > max1) {
+				max2 = max1;
+				max1 = num;
+			} else if (max2 == null || num > max2)
+				max2 = num;
+		}
+		Integer result = max2 == null ? max1 : max2;
+		System.out.println(result);
+	}
+
+}
