4c675cec2821ac06c6c704e8124fccf4d765b610
imjuoy <bsubhadeep95@gmail.com>
2018-07-02 23:17:40 -0400

    Random

--- src/SortList_MergeSort.java
+++ src/SortList_MergeSort.java
@@ -5,5 +5,6 @@ public class SortList_MergeSort {
 	public static void main(String[] args) {
 		int[] obj = new int[3];
 		System.out.println(Arrays.toString(obj));
+		System.out.println("abcd");
 	}
 }
